843a9c7f8897d3d3334360c3e9398acd08d02c1f
Hrushikesh <hrushikeshsuryawanshi832@gmail.com>
2026-03-08 15:09:26 +0530

    Resolved merge conflicts from Hrushikesh into main

--- backend/routes/grievanceRoutes.js
+++ backend/routes/grievanceRoutes.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD:backend/routes/grievanceRoutes.js
 const { createGrievance, getGrievances } = require('../controllers/grievanceCollector');
-=======
 const { createGrievance, getGrievances,analyzeComplaintController } = require('../controllers/grivienceCollector');
->>>>>>> Hrushikesh:backend/routes/grivienceRoutes.js
+
 
 router.route('/')
   .post(createGrievance)
